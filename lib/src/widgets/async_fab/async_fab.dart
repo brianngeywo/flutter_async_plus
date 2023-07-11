323f004b532290fa7b11789da3d05aeb5d095e6d
Arthur Miranda <60884977+arthurbcd@users.noreply.github.com>
2023-07-11 09:44:19 -0300

    async floating action button wip

--- lib/src/widgets/async_fab/async_fab.dart
+++ lib/src/widgets/async_fab/async_fab.dart
@@ -1,57 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/change_notifier.dart';
+import 'package:flutter_async/src/async_controller.dart';
+import 'package:flutter_async/src/widgets/async_state.dart';
 
 class AsyncFAB extends FloatingActionButton {
   const AsyncFAB({
-    Key? key,
-    required VoidCallback? onPressed,
-    MouseCursor? mouseCursor,
-    bool mini = false,
-    ShapeBorder? shape,
-    Clip clipBehavior = Clip.none,
-    Color? focusColor,
-    Color? hoverColor,
-    Color? splashColor,
-    double? disabledElevation,
-    double? focusElevation,
-    double? hoverElevation,
-    double? highlightElevation,
-    bool autofocus = false,
-    MaterialTapTargetSize? materialTapTargetSize,
-    bool? enableFeedback,
-    String? tooltip,
-    Color? foregroundColor,
-    Color? backgroundColor,
-    double? elevation,
-    FocusNode? focusNode,
-    String? heroTag,
-    bool isExtended = false,
-    required Widget child,
-  }) : super(
-          key: key,
-          onPressed: onPressed,
-          mouseCursor: mouseCursor,
-          mini: mini,
-          shape: shape,
-          clipBehavior: clipBehavior,
-          focusColor: focusColor,
-          hoverColor: hoverColor,
-          splashColor: splashColor,
-          disabledElevation: disabledElevation,
-          focusElevation: focusElevation,
-          hoverElevation: hoverElevation,
-          highlightElevation: highlightElevation,
-          autofocus: autofocus,
-          materialTapTargetSize: materialTapTargetSize,
-          enableFeedback: enableFeedback,
-          tooltip: tooltip,
-          foregroundColor: foregroundColor,
-          backgroundColor: backgroundColor,
-          child: child,
-          elevation: elevation,
-          focusNode: focusNode,
-          heroTag: heroTag,
-          isExtended: isExtended,
-        );
+    super.key,
+    super.onPressed,
+    super.mouseCursor,
+    super.mini,
+    super.shape,
+    super.clipBehavior,
+    super.focusColor,
+    super.hoverColor,
+    super.splashColor,
+    super.disabledElevation,
+    super.focusElevation,
+    super.hoverElevation,
+    super.highlightElevation,
+    super.autofocus,
+    super.materialTapTargetSize,
+    super.enableFeedback,
+    super.tooltip,
+    super.foregroundColor,
+    super.backgroundColor,
+    super.elevation,
+    super.focusNode,
+    super.heroTag,
+    super.child,
+    super.isExtended,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -75,11 +53,35 @@ class AsyncFAB extends FloatingActionButton {
       tooltip: tooltip,
       foregroundColor: foregroundColor,
       backgroundColor: backgroundColor,
-      child: child,
       elevation: elevation,
       focusNode: focusNode,
       heroTag: heroTag,
       isExtended: isExtended,
+      child: child,
     );
+  }
+}
+
+class FabAsync extends StatefulWidget implements AsyncWidget {
+  const FabAsync({
+    super.key,
+    this.controller,
+    this.listenables = const [],
+  });
+
+  @override
+  State<FabAsync> createState() => _FabAsyncState();
+
+  @override
+  final AsyncController? controller;
+
+  @override
+  final List<ValueListenable<bool>> listenables;
+}
+
+class _FabAsyncState extends State<FabAsync> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
